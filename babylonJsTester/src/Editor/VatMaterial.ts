3dbbc652666d560a9f2b3924c337be08e70e5d2f
mori <mori.norabadi@gmail.com>
2025-03-09 17:40:34 +0330

    read metadata and write them into uv maps

--- babylonJsTester/src/Editor/VatMaterial.ts
+++ babylonJsTester/src/Editor/VatMaterial.ts
@@ -6,7 +6,7 @@ export default class VatMaterial extends ShaderMaterial {
         const { scene, assetManager } = Editor.GetInstance()
 
         super(name, scene, "vat", {
-            attributes: ["position", "normal", "uv", "uv2"],
+            attributes: ["position", "normal", "uv", "uv2","uv3","uv4","uv5"],
             defines: ["#define INSTANCES"],
             uniforms: ["worldViewProjection", "posTex", "frame"]
         });
@@ -17,6 +17,9 @@ export default class VatMaterial extends ShaderMaterial {
                 attribute vec3 position;
                 attribute vec2 uv;
                 attribute vec2 uv2;
+                attribute vec2 uv3;
+                attribute vec2 uv4;
+                attribute vec2 uv5;
 
                 uniform mat4 worldViewProjection;
                 uniform sampler2D posTex;
@@ -25,106 +28,23 @@ export default class VatMaterial extends ShaderMaterial {
                 uniform float posTexWidth;
                 uniform float posTexHeight;
 
-                varying vec2 Vuv;
-                varying vec2 Vuv2;
-
-                // Denormalize 8-bit color channels to integers in the range 0 to 255.
-                ivec4 floatsToBytes(vec4 inputFloats, bool littleEndian) {
-                    ivec4 bytes = ivec4(inputFloats * 255.0);
-                    return (
-                      littleEndian
-                      ? bytes.abgr
-                      : bytes
-                    );
-                }
-                
-                // Break the four bytes down into an array of 32 bits.
-                void bytesToBits(const in ivec4 bytes, out bool bits[32]) {
-                    for (int channelIndex = 0; channelIndex < 4; ++channelIndex) {
-                        float acc = float(bytes[channelIndex]);
-                        for (int indexInByte = 7; indexInByte >= 0; --indexInByte) {
-                        float powerOfTwo = exp2(float(indexInByte));
-                        bool bit = acc >= powerOfTwo;
-                        bits[channelIndex * 8 + (7 - indexInByte)] = bit;
-                        acc = mod(acc, powerOfTwo);
-                        }
-                    }
-                }
-                
-                // Compute the exponent of the 32-bit float.
-                float getExponent(bool bits[32]) {
-                    const int startIndex = 1;
-                    const int bitStringLength = 8;
-                    const int endBeforeIndex = startIndex + bitStringLength;
-                    float acc = 0.0;
-                    int pow2 = bitStringLength - 1;
-                    for (int bitIndex = startIndex; bitIndex < endBeforeIndex; ++bitIndex) {
-                        acc += float(bits[bitIndex]) * exp2(float(pow2--));
-                    }
-                    return acc;
-                }
-                
-                // Compute the mantissa of the 32-bit float.
-                float getMantissa(bool bits[32], bool subnormal) {
-                    const int startIndex = 9;
-                    const int bitStringLength = 23;
-                    const int endBeforeIndex = startIndex + bitStringLength;
-                    // Leading/implicit/hidden bit convention:
-                    // If the number is not subnormal (with exponent 0), we add a leading 1 digit.
-                    float acc = float(!subnormal) * exp2(float(bitStringLength));
-                    int pow2 = bitStringLength - 1;
-                    for (int bitIndex = startIndex; bitIndex < endBeforeIndex; ++bitIndex) {
-                        acc += float(bits[bitIndex]) * exp2(float(pow2--));
-                    }
-                    return acc;
-                }
-                
-                // Parse the float from its 32 bits.
-                float bitsToFloat(bool bits[32]) {
-                    float signBit = float(bits[0]) * -2.0 + 1.0;
-                    float exponent = getExponent(bits);
-                    bool subnormal = abs(exponent - 0.0) < 0.01;
-                    float mantissa = getMantissa(bits, subnormal);
-                    float exponentBias = 127.0;
-                    return signBit * mantissa * exp2(exponent - exponentBias - 23.0);
-                }
-                
-                // Decode a 32-bit float from the RGBA color channels of a texel.
-                float rgbaToFloat(vec4 texelRGBA, bool littleEndian) {
-                    ivec4 rgbaBytes = floatsToBytes(texelRGBA, littleEndian);
-                    bool bits[32];
-                    bytesToBits(rgbaBytes, bits);
-                    return bitsToFloat(bits);
-                }
-
                 void main() {
-                    Vuv = uv;
-                    Vuv2 = uv2;
-
                     vec4 texturePos = texture2D(posTex,vec2(uv2.x, 1.0 - frame));
 
-                    vec4 pix0 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (0.5 / posTexHeight)));
-                    vec4 pix1 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (1.5 / posTexHeight)));
-                    vec4 pix2 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (2.5 / posTexHeight)));
-                    vec4 pix3 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (3.5 / posTexHeight)));
-                    vec4 pix4 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (4.5 / posTexHeight)));
-                    vec4 pix5 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (5.5 / posTexHeight)));
-                    vec4 pix6 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (6.5 / posTexHeight)));
-                    vec4 pix7 = texture2D(posTex,vec2(Vuv2.x, 1.0 - (7.5 / posTexHeight)));
-    
-                    float minX = rgbaToFloat(vec4(pix0.rgb,pix1.r), true);
-                    float maxX = rgbaToFloat(vec4(pix1.gb,pix2.rg), true);
+                    float minX = uv3.x;
+                    float maxX = uv3.y;
 
-                    float minY = rgbaToFloat(vec4(pix2.b,pix3.rgb), true);
-                    float maxY = rgbaToFloat(vec4(pix4.rgb,pix5.r), true);
+                    float minY = uv4.x;
+                    float maxY = uv4.y;
 
-                    float minZ = rgbaToFloat(vec4(pix5.gb,pix6.rg), true);
-                    float maxZ = rgbaToFloat(vec4(pix6.b,pix7.rgb), true);
+                    float minZ = uv5.x;
+                    float maxZ = uv5.y;
 
                     float posX = texturePos.x * (maxX - minX) + minX;
                     float posY = texturePos.y * (maxY - minY) + minY;
                     float posZ = texturePos.z * (maxZ - minZ) + minZ;
 
+                    // gl_Position = worldViewProjection * vec4(position.x,position.y,position.z, 1.0);
                     gl_Position = worldViewProjection * vec4(posX,posY,posZ, 1.0);
             }`;
 
@@ -147,12 +67,10 @@ export default class VatMaterial extends ShaderMaterial {
         this.setFloat("posTexWidth", size.width);
         this.setFloat("posTexHeight", size.height);
 
-        const max = (size.height - 8) / size.height
-        let a = 1 - max
-
+        let a = 0
         scene.onBeforeRenderObservable.add(() => {
             a += 0.002
-            if (a > max) a = 1 - max
+            if (a > 1) a = 0.0
             this.setFloat("frame", a)
         })
     }
--- babylonJsTester/src/Editor/VatMesh.ts
+++ babylonJsTester/src/Editor/VatMesh.ts
@@ -2,25 +2,44 @@ import { Mesh, VertexBuffer } from "@babylonjs/core";
 import Editor from "./Editor";
 import VatMaterial from "./VatMaterial";
 
+type MetaData = {
+    index: number;
+    min_x: number;
+    max_x: number;
+    min_y: number;
+    max_y: number;
+    min_z: number;
+    max_z: number;
+}
+
 export default class VatMesh {
     mesh: Mesh
     constructor(assetName: string) {
         const { assetManager } = Editor.GetInstance()
         this.mesh = assetManager.getInstance(assetName).rootNodes[0].getChildMeshes()[0] as Mesh;
-        this.setUV2()
+
+        this.processMetaData(this.mesh.metadata.gltf.extras.vertexData)
 
         this.mesh.material = new VatMaterial("test", "VAT_texture")
 
     }
 
-    private setUV2() {
-        const positions = this.mesh.getVerticesData(VertexBuffer.PositionKind)!
-        const count = positions.length
-        const uv2 = []
-        for (let i = 0; i < positions.length; i += 3) {
-            uv2.push(i / count, 0.0)
-        }
+    private processMetaData(data: MetaData[]) {
+        const uv2: number[] = [] // store index
+        const uv3: number[] = [] // store min max X
+        const uv4: number[] = [] // store min max X
+        const uv5: number[] = [] // store min max X
+
+        data.forEach(info => {
+            uv2.push(info.index, 0.0)
+            uv3.push(info.min_x, info.max_x)
+            uv4.push(info.min_y, info.max_y)
+            uv5.push(info.min_z, info.max_z)
+        })
 
         this.mesh.setVerticesData(VertexBuffer.UV2Kind, uv2)
+        this.mesh.setVerticesData(VertexBuffer.UV3Kind, uv3)
+        this.mesh.setVerticesData(VertexBuffer.UV4Kind, uv4)
+        this.mesh.setVerticesData(VertexBuffer.UV5Kind, uv5)
     }
 }
